89bbb9b197ab0b12106559e1a0b9610283280de2
Michael Anderson <michanders@msn.com>
2018-03-09 18:13:43 +0000

    automatically backed up by learn

--- account.js
+++ account.js
@@ -16,7 +16,7 @@ module.exports = function account() {
   }
 
   var signupBonus = 250,
-    deposit = 1000
+  var  deposit = 1000
 
   var openAccount = function(account, deposit) {
     account.balance += signupBonus
